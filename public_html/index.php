aec36f74940d335f4019d94c08f68b2fab817262
Jonathan SAHM <contact@johnstyle.fr>
2014-02-17 22:56:20 +0100

    Ajout Google Analytics

--- public_html/index.php
+++ public_html/index.php
@@ -119,5 +119,15 @@ Website::listenEvent(array(
         <script type="text/javascript" src="bower_components/jquery.qrcode/src/jquery.qrcode.js"></script>
         <script type="text/javascript" src="js/website.js"></script>
     <?php endif; ?>
+    <?php if(defined('GOOGLE_ANALYTICS')): ?>
+        <script type="text/javascript">
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+            ga('create', '<?php echo GOOGLE_ANALYTICS; ?>', '<?php echo $_SERVER['REMOTE_HOST']; ?>');
+            ga('send', 'pageview');
+        </script>
+    <?php endif; ?>
 </body>
 </html>
